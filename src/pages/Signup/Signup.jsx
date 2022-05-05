c53ae147d7a025bb9c0a13cf13c95084f8420f2f
giamazonas <skibinskirenee@gmail.com>
2022-05-05 12:20:06 -0400

    save work to remote

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import SignupForm from '../../components/SignupForm/SignupForm'
-import styles from './Signup.module.css'
+import './signup.css'
 
 const Signup = props => {
   const [message, setMessage] = useState([''])
@@ -10,10 +10,15 @@ const Signup = props => {
   }
 
   return (
-    <main className={styles.container}>
-      <h1>Sign Up</h1>
+    <main className="create-account-container">
+      <h1 className='create-account-title'>Create an account</h1>
       <p>{message}</p>
-      <SignupForm {...props} updateMessage={updateMessage} />
+      <div className="create-account-area">
+        <h3 className='create-account-salutation'>Welcome</h3>
+        <p className='create-account-salutation-subtitle'>Hello, create an account to continue!</p>
+        <p>{message}</p>
+        <SignupForm {...props} updateMessage={updateMessage} />
+      </div>
     </main>
   )
 }
