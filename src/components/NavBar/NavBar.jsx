1985a2de8543263c44fe5b66f1ecd0ab5f520305
Jamel Scott Fadel <itsjamelscott@gmail.com>
2022-05-04 17:37:11 -0700

    adjustments to links in nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
 import { slide as Menu } from 'react-burger-menu'
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-import logo from './logo.svg'
+import Logo from './logo.svg'
+import Favorite from './favorite.svg'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './navbar.css'
 const NavBar = ({ user, handleLogout }) => {
@@ -12,59 +13,46 @@ const NavBar = ({ user, handleLogout }) => {
 
 
   return (
-    <nav class="navbar navbar-light bg-lignt">
-    <a href="/" class="navbar-brand">Light</a>
-    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
-        <span class="navbar-toggler-icon my-toggler"></span>
-    </button>
-    <div class="navbar-collapse collapse" id="collapsingNavbar2">
-        <ul class="navbar-nav mx-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-    // <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
-    //   <Navbar.Brand href="#home">
-    //     <div>
-    //       <img src={logo}></img>
-    //     </div>
-    //   </Navbar.Brand>
-    //   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    //   <Navbar.Collapse id="responsive-navbar-nav">
-    //     <Nav className="mr-auto">
-    //       <Nav.Link href="#features">Features</Nav.Link>
-    //       <Nav.Link href="#pricing">Pricing</Nav.Link>
-    //       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-    //         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-    //         <NavDropdown.Item href="#action/3.2">
-    //           Another action
-    //         </NavDropdown.Item>
-    //         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-    //         <NavDropdown.Divider />
-    //         <NavDropdown.Item href="#action/3.4">
-    //           Separated link
-    //         </NavDropdown.Item>
-    //       </NavDropdown>
-    //     </Nav>
-    //     <Nav>
-    //       <Nav.Link href="#deets">More deets</Nav.Link>
-    //       <Nav.Link eventKey={2} href="#memes">
-    //         Dank memes
-    //       </Nav.Link>
-    //     </Nav>
-    //   </Navbar.Collapse>
-    // </Navbar>
+    <Navbar style={{padding: "0 10px"}} collapseOnSelect expand="lg" bg="light" variant="light">
+
+      <Navbar.Brand href="#home">
+        <img src={Logo}></img>
+
+
+      </Navbar.Brand>
+      <div style={{display: "flex", gap: "20px", alignItems: "center"}} >
+      <div>
+        <img src={Favorite}></img>
+      </div>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+      </div>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        {user ? 
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/profiles">Profiles</Nav.Link>
+          <Nav.Link href="/resources">Resources</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link onClick={handleLogout}>Log out</Nav.Link>
+        </Nav>
+        :
+        <Nav className="mr-auto">
+        <Nav.Link href="/login">Log In</Nav.Link>
+        <Nav.Link href="/signup">Sign Up</Nav.Link>
+        <Nav.Link href="/resources">Resources</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/jobform">Post a job</Nav.Link>
+      </Nav>
+        }
+      </Navbar.Collapse>
+    </Navbar>
     // <>
     //   {user ?
     //     <nav>
     //       <div>
     //         <h3>Welcome, {user.name}</h3>
-    //         <Link className="menu-item" to="/"><h3>Home</h3></Link>
+    //         <Link className="menu-item" to="/">Home</Link>
     //         <Link className="menu-item" to="/profiles"><h3>Profiles</h3></Link>
     //         <Link className="menu-item" to="" onClick={handleLogout}><h3>Log out</h3></Link>
     //         <Link className="menu-item" to="/resources"><h3>Resources</h3></Link>
