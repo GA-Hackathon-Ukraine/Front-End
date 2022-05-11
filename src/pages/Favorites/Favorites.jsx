37daeb023a3f0c34e62709d7cb9763169632dc26
Jamel Scott Fadel <itsjamelscott@gmail.com>
2022-05-10 17:02:59 -0700

    still still troubleshooting

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,83 +1,108 @@
 import { Link } from 'react-router-dom';
 import constructionIcon from '../../../src/construction.svg';
 import technologyIcon from '../../../src/technology.svg';
-import foodandbeveragesIcon from '../../../src/food.svg'
+import foodandbeveragesIcon from '../../../src/food.svg';
 import locationIcon from '../../../src/location.svg';
 import messageIcon from '../../../src/message.svg';
 import savedfilled from '../../../src/savedred.svg';
+import transportationIcon from '../../../src/transportation.svg';
+import caretakingIcon from '../../../src/caretaking.svg';
+import cleaningIcon from '../../../src/cleaning.svg';
+import educationIcon from '../../../src/education.svg';
+
 import { useAuth } from '../../utils/auth';
 import { useEffect } from 'react';
 import { supabase } from '../../utils/supabaseClient';
 import { useState } from 'react';
 
-
 const Favorites = ({ favorites }) => {
+  const auth = useAuth();
 
-  const auth = useAuth()
+  const [favJobs, setFavJobs] = useState([]);
 
-  
-  const [favJobs, setFavJobs] = useState([])
-  
-  let favoritedJobs = [] 
+  let favoritedJobs = []
 
-  const getFavorites = async () =>{
+  const getFavorites = async () => {
+    const { data, error } = await supabase
+      .from('Favorites')
+      .select()
+      .match({ user_id: auth.user.id });
 
-    const {data, error} = await supabase.from("Favorites").select().match({user_id: auth.user.id})
+    data.forEach(async (job, idx) => {
+      console.log(job);
+      const { data, error } = await supabase
+        .from('Jobs')
+        .select()
+        .match({ id: job.job_id });
 
+      if (error) {
+        console.log(error);
+      }
 
-    data.forEach(async(job, idx)=>{
-
-      console.log(job)
-      const {data, error} = await supabase.from("Jobs").select().match({id: job.job_id})
-      
-      if (error){console.log(error)}
-    
       // console.log(data[0])
-      favoritedJobs.push(data[0])
-      setFavJobs(favoritedJobs)
-
-    })
-    
+      favoritedJobs.push(data[0]);
+      // setFavJobs([...favJobs, data[0]]);
+    });
     // for(const job in data){
       //   const {data, error} = await supabase.from("Jobs").select().match({id: job})
       //   // console.log(data[0])
       //   favoritedJobs.push(data[0])
       
-      
       // }
       // console.log(favoritedJobs)
-      if(error){
-        console.log(error)
-      } 
-    }
+      if (error) {
+        console.log(error);
+      }
+    };
     
-    useEffect(()=>{
-      
-      getFavorites()
-      
+    useEffect(() => {
+      getFavorites();
+      setFavJobs(favoritedJobs)
 
+  }, []);
 
-  },[])
-
-
-  console.log(favoritedJobs)
-  const mappedData = favoritedJobs.map((elem, idx) => {
-
-    {console.log(elem)}
+  const mappedData = favJobs.map((elem, idx) => {
     let industry;
     switch (elem.industry) {
-      case 'technology':
+      case 'Technology':
         industry = technologyIcon;
         break;
-      case 'food/beverage':
+      case 'Food/Beverages':
         industry = foodandbeveragesIcon;
+        break;
+      case 'Transportation':
+        industry = transportationIcon;
+        break;
+      case 'Caretaking':
+        industry = caretakingIcon;
+        break;
+      case 'Education':
+        industry = educationIcon;
+        break;
+      case 'Cleaning':
+        industry = cleaningIcon;
+        break;
+      case 'Gardening':
+        industry = cleaningIcon;
+        break;
+      case 'Other':
+        industry = cleaningIcon;
         break;
       default:
         industry = constructionIcon;
     }
     console.log(industry);
     return (
-      <div key={idx} style={{margin: "0 auto", padding: '10px', gap: "30px", marginBottom: "20px"}} className="card-wrapper">
+      <div
+        key={idx}
+        style={{
+          margin: '0 auto',
+          padding: '10px',
+          gap: '30px',
+          marginBottom: '20px',
+        }}
+        className="card-wrapper"
+      >
         <Link
           className="card-wrapper-container"
           key={`job-${idx}`}
@@ -112,17 +137,19 @@ const Favorites = ({ favorites }) => {
             <div className="card-wrapper-info-top">
               <div className="card-wrapper-info-top-header">
                 <div>
-                  <h2 style={{paddingLeft: "0"}} className="position">{elem.position}</h2>
+                  <h2 style={{ paddingLeft: '0' }} className="position">
+                    {elem.position}
+                  </h2>
                 </div>
                 <div className="hourly-rate">
                   <h3 className="compensation">${elem.compensation}</h3>
                   <span className="per-hour">/Hour</span>
                 </div>
               </div>
-              <div  className="card-wrapper-info-top-lower">
+              <div className="card-wrapper-info-top-lower">
                 <div>
                   {' '}
-                  <h4  className="company-jobsmap">{elem.company}</h4>{' '}
+                  <h4 className="company-jobsmap">{elem.company}</h4>{' '}
                 </div>
                 <div className="location-wrapper-jobsmap">
                   <img className="img-jobsmap" src={locationIcon} />
@@ -168,8 +195,12 @@ const Favorites = ({ favorites }) => {
     );
   });
 
-  return <div style={{ paddingTop: "75px"}}>
-    <h1 style={{textAlign: "center", color: "white"}}>Saved Jobs</h1>{mappedData}</div>;
+  return (
+    <div style={{ paddingTop: '75px' }}>
+      <h1 style={{ textAlign: 'center', color: 'white' }}>Saved Jobs</h1>
+      {mappedData}
+    </div>
+  );
 };
 
 export default Favorites;
