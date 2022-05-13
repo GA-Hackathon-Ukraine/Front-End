bf5c94a213e0fc76d6ee4a5b34cb4b04cae77b5b
“jamelscott” <itsjamelscott@gmail.com>
2022-05-13 14:49:26 -0700

    adjusted style on choose location

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,33 +1,43 @@
+import "./SearchForm.css";
+import locationIcon from "./locationicon.png";
+import searchIcon from "./searchicon.png";
 
-import './SearchForm.css';
-import locationIcon from './locationicon.png'
-import searchIcon from './searchicon.png'
-
-function SearchForm({setLocation, position, setPosition, setSearchShow}) {
-
-    const handleChange = (e) => {
-        setPosition(e.target.value)   
-    }
+function SearchForm({ setLocation, position, setPosition, setSearchShow }) {
+  const handleChange = (e) => {
+    setPosition(e.target.value);
+  };
 
   const handleSetLocation = (e) => {
     setLocation(e.target.value);
   };
 
   const handleClickRender = () => {
-    setSearchShow(true)
-  }
-    
+    setSearchShow(true);
+  };
+
   return (
     <div className="searchContainer">
-      <form className='searchContainer-form' onClick={handleClickRender}>
-        <div className='searchContainerLocation'>
-        <img className='searchContainer-searchIcon' src={searchIcon}/>
-        
-          <input className='searchInput' placeholder="Search" type="text" onChange={handleChange} value={position}/>
+      <form className="searchContainer-form" onClick={handleClickRender}>
+        <div className="searchContainerLocation">
+          <img className="searchContainer-searchIcon" src={searchIcon} />
+
+          <input
+            className="searchInput"
+            placeholder="Search"
+            type="text"
+            onChange={handleChange}
+            value={position}
+          />
         </div>
-        <div className='searchContainerSearch'>
-        <img className='searchContainer-locationIcon' src={locationIcon}/>
-          <select defaultValue={'DEFAULT'} className='locationInput' onChange={handleSetLocation} type="text">
+        <div className="searchContainerSearch">
+          <img className="searchContainer-locationIcon" src={locationIcon} />
+          <select
+            defaultValue={"DEFAULT"}
+            className="locationInput"
+            onChange={handleSetLocation}
+            type="text"
+            style={{ color: "grey" }}
+          >
             <option value="DEFAULT" disabled hidden>
               Choose Location
             </option>
@@ -47,7 +57,6 @@ function SearchForm({setLocation, position, setPosition, setSearchShow}) {
       </form>
     </div>
   );
-
 }
 
 export default SearchForm;
