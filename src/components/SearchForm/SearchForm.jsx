9108eb096136b0f89a8cb99a18949ce96c2a4683
romanprotoliuk <romanprotoliuk@gmail.com>
2022-05-13 15:36:30 -0700

    refactored job card component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,12 +5,16 @@ import searchIcon from './searchicon.png'
 
 function SearchForm({setLocation, position, setPosition, setSearchShow}) {
 
-    const handleChange = (e) => {
-        setPosition(e.target.value)   
-    }
+  const handleChange = (e) => {
+    setPosition(e.target.value)   
+  }
 
   const handleSetLocation = (e) => {
-    setLocation(e.target.value);
+    if (e.target.value === "All") {
+      setLocation('')
+    } else {
+      setLocation(e.target.value);
+    }
   };
 
   const handleClickRender = () => {
@@ -31,6 +35,7 @@ function SearchForm({setLocation, position, setPosition, setSearchShow}) {
             <option value="DEFAULT" disabled hidden>
               Choose Location
             </option>
+            <option>All</option>
             <option>Tacoma</option>
             <option>Seattle</option>
             <option>Olympia</option>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -64,6 +64,7 @@ const Landing = ({ searchShow, setSearchShow }) => {
         setPosition={setPosition}
         setSearchShow={setSearchShow}
         searchShow={searchShow}
+        allJobs={allJobs}
       />
 
       {searchShow ? (
